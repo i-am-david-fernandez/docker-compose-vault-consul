997dd0da5f6934946f549d76958ebe8c0cecfa90
David Fernandez <i.am.david.fernandez@gmail.com>
2016-09-06 16:28:05 +1000

    <DF> Added script to generate non-root tokens.

---
+++ create_tokens.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source vault.cfg
+
+export VAULT_ADDR=$vault_host_addr
+
+echo $*
+
+if which vault > /dev/null
+then
+    echo "Operating via CLI API."
+    vault auth $vault_root_token
+
+    for token_name in $*
+    do
+        ## Create a non-root token
+        echo "Creating a token with display name $token_name"
+        pattern_token="token.*"
+
+        lines=`vault token-create -display-name=$token_name`
+        IFS_org=$IFS
+        IFS=$'\n'
+        for line in $lines
+        do
+            echo $line
+            if  [[ $line =~ $pattern_token ]]
+            then
+                label=`echo $line | awk '{ print $1 }'`
+                value=`echo $line | awk '{ print $2 }'`
+                echo "${token_name}_$label=\"$value\"" >> tokens.cfg
+            fi
+
+        done
+        IFS=$IFS_org
+    done
+else
+    echo "Vault CLI binary not found. Cannot operate via CLI API."
+fi
+
